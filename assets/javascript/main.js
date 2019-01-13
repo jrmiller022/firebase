324ba3730dbaa856eef0117eb4efe44390c55d54
Jaybird <jrmiller022@gmail.com>
2019-01-13 13:48:35 -0600

    update

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -26,9 +26,16 @@ firebase.initializeApp(config);
   
       // Grabbed values from text-boxes
       name = $("#name-input").val().trim();
+      $("#name-input").val("");
+
       destination = $("#destination-input").val().trim();
+      $("#destination-input").val("");
+
       firstTrainTime = $("#firstTrainTime-input").val().trim();
+      $("#firstTrainTime").val("");
+
       frequency = $("#frequency-input").val().trim();
+      $("#frequency-input").val("");
   
       // Code for "Setting values in the database"
       database.ref().push({
@@ -44,15 +51,16 @@ firebase.initializeApp(config);
   
     // Firebase watcher + initial loader
     database.ref().on("child_added", function(childSnapshot) {
-        var train = childSnapshot.val().name;
-        var destination = childSnapshot.val().destination;
-        var frequency = childSnapshot.val().frequency; 
-        var firstTrainTime = childSnapshot.val().firstTrainTime;
-        var trainRow = $('<tr>');
-        var trainCell = $('<td>').text(train.name);
+        var train = childSnapshot.val.name;
+
+        destination = childSnapshot.val.destination;
+
+        frequency = childSnapshot.val.frequency; 
+
+        firstTrainTime = childSnapshot.val.firstTrainTime;
   
-        trainRow.append(trainCell);
-        $('#name-display').append(trainRow)
+        // trainRow.append(trainCell);
+        // $('#name-display').append(trainRow)
 
       // Log everything that's coming out of snapshot
       console.log(childSnapshot.val());
@@ -79,6 +87,10 @@ firebase.initializeApp(config);
 
   // New variable
   var trainFreq;
+  var diffTime;
+  var tRemainder;
+  var nextTrainTime;
+  var minutesAway;
 
   // Train time entered on the entry form
     var firstTime = 0;
@@ -92,48 +104,39 @@ firebase.initializeApp(config);
 
   // Difference between the times
   var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-  console.log("DIFFERENCE IN TIME: " + diffTime);
 
   // Time apart (remainder)
     var tRemainder = diffTime % trainFreq;
-    console.log(tRemainder);
 
     // Minutes Until Train
     var tMinutesTillTrain = frequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
     // Train arrival next
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("HH:mm A");
+
+    //Change the HTML to reflect the inputs into the table
+    var tableRow = $("<tr>");
+    var tableTrainName = $("<td>");
+    var tableDestination = $("<td>");
+    var tableFrequency = $("<td>");
+    var tableTrainTime = $("<td>");
+    var tableMinutesAway = $("<td>");
+
+    // Creates table data for each input.
+    $("tbody").append(tableRow);
+    tableRow.append(tableTrainName);
+    tableRow.append(tableDestination);
+    tableRow.append(tableFrequency);
+    tableRow.append(tableTrainTime);
+    tableRow.append(tableMinutesAway);
 
 
-  // Add each train's data into the table
-  $("#train-table > tbody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + 
-   "</td><td>" + moment(nextTrain).format("HH:mm") + "</td><td>" + tMinutesTillTrain + "</td></tr>");
-  });
-
-    // Create the new row
-    var newRow = $("<tr>").append(
-      $("<td>").text(name),
-      // $("<td>").text(destination),
-      // $("<td>").text(frequency),
-      // $("<td>").text(nextTrain),
-      // $("<td>").text(minutesAway),
-    );
+    // Appends the value to the table in single row
+    tableTrainName.append(name);
+    tableDestination.append(destination);
+    tableFrequency.append(frequency);
+    tableTrainTime.append(nextTrainTime);
+    tableMinutesAway.append(minutesAway); 
+  })
   
-    // Append the new row to the table
-    $(".table > tbody").append(newRow);
-  
-    // Time clock
-	function currentTime() {
-		var sec = 1;	
-		time = moment().format('HH:mm:ss');
-		searchTime = moment().format('HH:mm');
-			$('#currentTime').html(time);
 
-			t = setTimeout(function() {
-				currentTime();
-			}, sec * 1000);	
-	}
-  currentTime(); 
-  
